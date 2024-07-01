119286341677108143d662d807e235ba18db8c95
Joshua Fritzjunker <fritzjunker69@gmail.com>
2024-07-01 15:13:50 -0500

    Update dmm_shortcuts.py

--- dmm_shortcuts.py
+++ dmm_shortcuts.py
@@ -2,6 +2,18 @@ import os
 import shutil
 
 
+def clear_directory(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+
+
 def create_custom_file(directory, source_filepath):
     if not os.path.exists(directory):
         print(f"Directory '{directory}' does not exist.")
@@ -44,7 +56,51 @@ if __name__ == "__main__":
             r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Fargo_Prod.4DLink",
             r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Fargo_Test.4DLink",
         ],
+        "goodfield": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Goodfield_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Goodfield_Test.4DLink",
+        ],
+        "grand island": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Grand_Island_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Grand_Island_Test.4DLink",
+        ],
+        "new holland": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\New_Holland_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\New_Holland_Test.4DLink",
+        ],
+        "queretaro": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Queretaro_Prod.4DLink",
+        ],
+        "racine": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Racine_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Racine_Test.4DLink",
+        ],
+        "saskatoon": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Saskatoon_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Saskatoon_Test.4DLink",
+        ],
         "wichita": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Wichita_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Wichita_Test.4DLink",
+        ],
+        "all": [
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Benson_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Benson_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Burlington_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Burlington_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Fargo_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Fargo_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Goodfield_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Goodfield_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Grand_Island_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Grand_Island_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\New_Holland_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\New_Holland_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Queretaro_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Racine_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Racine_Test.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Saskatoon_Prod.4DLink",
+            r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Saskatoon_Test.4DLink",
             r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Wichita_Prod.4DLink",
             r"\\cnh1\bur\BUR_Kitting\FritzjunkerDeploy\DMM_Shortcuts\Wichita_Test.4DLink",
         ],
@@ -62,6 +118,8 @@ if __name__ == "__main__":
     if selection in plant_files:
         source_filepaths = plant_files[selection]
         if os.path.exists(user_directory):
+            # Clear the user directory before creating new files
+            clear_directory(user_directory)
             for source_filepath in source_filepaths:
                 create_custom_file(user_directory, source_filepath)
         else:
